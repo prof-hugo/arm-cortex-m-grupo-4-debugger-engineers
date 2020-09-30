8d4cd7bab6176d6d2d62c95f336b8ae623afbc2c
Luís Henrique Beltrão Santana <beltraoluis@gmail.com>
2020-09-30 07:24:12 -0300

    blink  2b extra keil

--- keil/tasks_FreeRTOS/main.c
+++ keil/tasks_FreeRTOS/main.c
@@ -1,35 +1,38 @@
-#include <stdint.h>
 #include <FreeRTOS.h>
 #include <task.h>
+#include "LED.h"
 
-void vPeriodicTask(void *pvParameters)
-{
-	
-	// Establish the task's period.
-	const TickType_t xDelay = pdMS_TO_TICKS(1000);
-	TickType_t xLastWakeTime = xTaskGetTickCount();
-	
-	for (;;) {
-
-		
-		// Block until the next release time.
-		vTaskDelayUntil(&xLastWakeTime, xDelay);
-	}
-
-}	
-
-
+typedef struct xblink{
+  int led;
+	int timeMs;
+}xBlink;
+void vBlinkTask(void *pvParameters);
 
 int main()
 {
-	
-	xTaskCreate(vPeriodicTask, "My Task", 256, NULL, 1, NULL);
+	xBlink xB0 = {0, 100}; 
+	xBlink xB1 = {1, 200}; 
+	xBlink xB2 = {2, 300}; 
+	xBlink xB3 = {3, 400};
 
-	// Startup of the FreeRTOS scheduler.  The program should block here.  
+	LED_Initialize();
+	xTaskCreate(vBlinkTask, "Blink0", configMINIMAL_STACK_SIZE, &xB0, 1, NULL);
+	xTaskCreate(vBlinkTask, "Blink1", configMINIMAL_STACK_SIZE, &xB1, 1, NULL);
+	xTaskCreate(vBlinkTask, "Blink2", configMINIMAL_STACK_SIZE, &xB2, 1, NULL);
+	xTaskCreate(vBlinkTask, "Blink3", configMINIMAL_STACK_SIZE, &xB3, 1, NULL);
 	vTaskStartScheduler();
-	
-	// The following line should never be reached.  Failure to allocate enough
-	//	memory from the heap would be one reason.
-	for (;;);
-	
+	while(1);
 }
+
+void vBlinkTask(void *pvParameters)
+{
+	xBlink *xB = (xBlink *) pvParameters;
+	const TickType_t xDelay = pdMS_TO_TICKS(xB->timeMs);
+	TickType_t xLastWakeTime = xTaskGetTickCount();
+	while(1) {
+		LED_On(xB->led);
+		vTaskDelayUntil(&xLastWakeTime, xDelay);
+		LED_Off(xB->led);
+		vTaskDelayUntil(&xLastWakeTime, xDelay);
+	}
+}	
