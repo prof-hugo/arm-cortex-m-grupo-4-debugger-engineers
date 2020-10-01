1818c3a38807528c354c67b21ab9c0f6d14d7b36
Luís Henrique Beltrão Santana <beltraoluis@gmail.com>
2020-09-30 21:14:49 -0300

    exercicio 2a

--- keil/tasks_FreeRTOS/main.c
+++ keil/tasks_FreeRTOS/main.c
@@ -2,37 +2,66 @@
 #include <task.h>
 #include "LED.h"
 
-typedef struct xblink{
-  int led;
-	int timeMs;
-}xBlink;
-void vBlinkTask(void *pvParameters);
-
-xBlink xB0 = {0, 530}; 
-xBlink xB1 = {1, 430}; 
-xBlink xB2 = {2, 330}; 
-xBlink xB3 = {3, 230};
+void vBlink0Task(void *pvParameters);
+void vBlink1Task(void *pvParameters);
+void vBlink2Task(void *pvParameters);
+void vBlink3Task(void *pvParameters);
 
 int main()
 {
 	LED_Initialize();
-	xTaskCreate(vBlinkTask, "Blink0", configMINIMAL_STACK_SIZE, &xB0, 1, NULL);
-	xTaskCreate(vBlinkTask, "Blink1", configMINIMAL_STACK_SIZE, &xB1, 1, NULL);
-	xTaskCreate(vBlinkTask, "Blink2", configMINIMAL_STACK_SIZE, &xB2, 1, NULL);
-	xTaskCreate(vBlinkTask, "Blink3", configMINIMAL_STACK_SIZE, &xB3, 1, NULL);
+	xTaskCreate(vBlink0Task, "Blink0", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
+	xTaskCreate(vBlink1Task, "Blink1", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
+	xTaskCreate(vBlink2Task, "Blink2", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
+	xTaskCreate(vBlink3Task, "Blink3", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
 	vTaskStartScheduler();
 	while(1);
 }
 
-void vBlinkTask(void *pvParameters)
+void vBlink0Task(void *pvParameters)
 {
-	xBlink *xB = (xBlink *) pvParameters;
-	const TickType_t xDelay = pdMS_TO_TICKS(xB->timeMs);
+	const TickType_t xDelay = pdMS_TO_TICKS(530);
 	TickType_t xLastWakeTime = xTaskGetTickCount();
 	while(1) {
-		LED_On(xB->led);
+		LED_On(0);
 		vTaskDelayUntil(&xLastWakeTime, xDelay);
-		LED_Off(xB->led);
+		LED_Off(0);
+		vTaskDelayUntil(&xLastWakeTime, xDelay);
+	}
+}
+
+void vBlink1Task(void *pvParameters)
+{
+	const TickType_t xDelay = pdMS_TO_TICKS(430);
+	TickType_t xLastWakeTime = xTaskGetTickCount();
+	while(1) {
+		LED_On(1);
+		vTaskDelayUntil(&xLastWakeTime, xDelay);
+		LED_Off(1);
+		vTaskDelayUntil(&xLastWakeTime, xDelay);
+	}
+}
+
+void vBlink2Task(void *pvParameters)
+{
+	const TickType_t xDelay = pdMS_TO_TICKS(330);
+	TickType_t xLastWakeTime = xTaskGetTickCount();
+	while(1) {
+		LED_On(2);
+		vTaskDelayUntil(&xLastWakeTime, xDelay);
+		LED_Off(2);
+		vTaskDelayUntil(&xLastWakeTime, xDelay);
+	}
+}
+
+void vBlink3Task(void *pvParameters)
+{
+	const TickType_t xDelay = pdMS_TO_TICKS(230);
+	TickType_t xLastWakeTime = xTaskGetTickCount();
+	while(1) {
+		LED_On(3);
+		vTaskDelayUntil(&xLastWakeTime, xDelay);
+		LED_Off(3);
 		vTaskDelayUntil(&xLastWakeTime, xDelay);
 	}
 }	
