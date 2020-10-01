68405a3a45eefd7f3931b16e2aeb170edfc6fd84
Luís Henrique Beltrão Santana <beltraoluis@gmail.com>
2020-09-30 21:04:37 -0300

    exercicio 3

--- keil/tasks_FreeRTOS/main.c
+++ keil/tasks_FreeRTOS/main.c
@@ -1,38 +1,16 @@
-#include <FreeRTOS.h>
-#include <task.h>
+#include "TM4C129.h"                    // Device header
 #include "LED.h"
-
-typedef struct xblink{
-  int led;
-	int timeMs;
-}xBlink;
-void vBlinkTask(void *pvParameters);
-
-xBlink xB0 = {0, 530}; 
-xBlink xB1 = {1, 430}; 
-xBlink xB2 = {2, 330}; 
-xBlink xB3 = {3, 230};
 
 int main()
 {
+	int i;
 	LED_Initialize();
-	xTaskCreate(vBlinkTask, "Blink0", configMINIMAL_STACK_SIZE, &xB0, 1, NULL);
-	xTaskCreate(vBlinkTask, "Blink1", configMINIMAL_STACK_SIZE, &xB1, 1, NULL);
-	xTaskCreate(vBlinkTask, "Blink2", configMINIMAL_STACK_SIZE, &xB2, 1, NULL);
-	xTaskCreate(vBlinkTask, "Blink3", configMINIMAL_STACK_SIZE, &xB3, 1, NULL);
-	vTaskStartScheduler();
-	while(1);
-}
-
-void vBlinkTask(void *pvParameters)
-{
-	xBlink *xB = (xBlink *) pvParameters;
-	const TickType_t xDelay = pdMS_TO_TICKS(xB->timeMs);
-	TickType_t xLastWakeTime = xTaskGetTickCount();
-	while(1) {
-		LED_On(xB->led);
-		vTaskDelayUntil(&xLastWakeTime, xDelay);
-		LED_Off(xB->led);
-		vTaskDelayUntil(&xLastWakeTime, xDelay);
+	while(1){
+		LED_On(0);
+		LED_On(1);
+		for(i=0;i<700000;i++);
+		LED_Off(0);
+		LED_Off(1);
+		for(i=0;i<700000;i++);
 	}
-}	
+}
