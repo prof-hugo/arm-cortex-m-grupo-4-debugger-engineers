66ece46f13a1465a645c0bd3121fedb1bc4473e2
GabGanzert <62157022+GabGanzert@users.noreply.github.com>
2020-10-03 10:58:06 -0300

    Update tasks_FreeRTOS_qemu.c
    Tarefa 2.b: criação de uma tarefa genérica para fazer um led piscar, utilizando 4 instâncias da mesma para 4 leds diferentes com períodos diferentes.

--- qemu/tasks_FreeRTOS_qemu/tasks_FreeRTOS_qemu.c
+++ qemu/tasks_FreeRTOS_qemu/tasks_FreeRTOS_qemu.c
@@ -13,36 +13,50 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
+typedef struct T_led{
+	Led_TypeDef led;
+	int time;
+} Led_type;
 
-void vBlinkLED4(void *pvParam){
-  const TickType_t xDelay = 500 / portTICK_PERIOD_MS;
+Led_type l3, l4, l5, l6;
 
-  while(1){
-    BSP_LED_Toggle(LED4);
-    vTaskDelay(xDelay);
-  } // while
-} // vBlinkLED4
+void vBlinkLED(void *pvParam){
 
+  Led_type *led_t=(Led_type *) pvParam;
 
-void vBlinkLED5(void *pvParam){
   TickType_t xLastWakeTime;
-  const TickType_t xPeriod = 500 / portTICK_PERIOD_MS;
+  const TickType_t xPeriod = (*led_t).time / portTICK_PERIOD_MS;
   xLastWakeTime = xTaskGetTickCount();
 
   while(1){
-    BSP_LED_Toggle(LED5);
-    vTaskDelayUntil(&xLastWakeTime, xPeriod);
+	BSP_LED_Toggle((*led_t).led);
+	vTaskDelayUntil(&xLastWakeTime, xPeriod);
   } // while
-} // vBlinkLED5
-
+} // vBlinkLED
 
 void main(void){
   __HAL_RCC_GPIOD_CLK_ENABLE();
+  BSP_LED_Init(LED3);
   BSP_LED_Init(LED4);
   BSP_LED_Init(LED5);
+  BSP_LED_Init(LED6);
 
-  xTaskCreate(vBlinkLED4, "vBlinkLED4", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY, NULL);
-  xTaskCreate(vBlinkLED5, "vBlinkLED5", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY, NULL);
+  l3.led=LED3;
+  l3.time=530;
+
+  l4.led=LED4;
+  l4.time=430;
+
+  l5.led=LED5;
+  l5.time=330;
+
+  l6.led=LED6;
+  l6.time=230;
+
+  xTaskCreate(vBlinkLED, "vBlinkLED3", configMINIMAL_STACK_SIZE, &l3, tskIDLE_PRIORITY, NULL);
+  xTaskCreate(vBlinkLED, "vBlinkLED4", configMINIMAL_STACK_SIZE, &l4, tskIDLE_PRIORITY, NULL);
+  xTaskCreate(vBlinkLED, "vBlinkLED5", configMINIMAL_STACK_SIZE, &l5, tskIDLE_PRIORITY, NULL);
+  xTaskCreate(vBlinkLED, "vBlinkLED6", configMINIMAL_STACK_SIZE, &l6, tskIDLE_PRIORITY, NULL);
 
   vTaskStartScheduler();
   while(1);
