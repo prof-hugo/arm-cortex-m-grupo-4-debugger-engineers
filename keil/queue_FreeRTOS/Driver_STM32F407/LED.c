bfb717bd90e5e14d8f3f28df73cb8834ebaac150
Luís Henrique Beltrão Santana <beltraoluis@gmail.com>
2020-10-19 19:23:29 -0300

    multiplataforma

---
+++ keil/queue_FreeRTOS/Driver_STM32F407/LED.c
@@ -0,0 +1,84 @@
+#include "stm32f4xx_hal.h"              // Keil::Device:STM32Cube HAL:Common
+#include "LED.h"
+
+const uint32_t led_mask[] = { 1UL << 1, 1UL << 0, 1UL << 4, 1UL << 0  };
+
+/*----------------------------------------------------------------------------
+  initialize LED Pins (GPION1, GPION0, GPIOF0, GPIOF4)
+ *----------------------------------------------------------------------------*/
+void LED_Initialize (void) {
+
+	GPIO_InitTypeDef GPIO_InitStruct;
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : PD12 PD13 PD14 PD15 */
+  GPIO_InitStruct.Pin = GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);	
+
+}
+
+/*----------------------------------------------------------------------------
+  Function that turns on requested LED
+ *----------------------------------------------------------------------------*/
+void LED_On (uint32_t num) {
+		if (num < LED_NUM) {
+    switch (num) {
+      case 0: HAL_GPIO_WritePin(GPIOD,GPIO_PIN_12,1); break;
+      case 1: HAL_GPIO_WritePin(GPIOD,GPIO_PIN_13,1); break;
+      case 2: HAL_GPIO_WritePin(GPIOD,GPIO_PIN_14,1); break;
+      case 3: HAL_GPIO_WritePin(GPIOD,GPIO_PIN_15,1); break;
+    }
+  }
+}
+
+/*----------------------------------------------------------------------------
+  Function that turns off requested LED
+ *----------------------------------------------------------------------------*/
+void LED_Off (uint32_t num) {
+	if (num < LED_NUM) {
+    switch (num) {
+      case 0: HAL_GPIO_WritePin(GPIOD,GPIO_PIN_12,0); break;
+      case 1: HAL_GPIO_WritePin(GPIOD,GPIO_PIN_13,0); break;
+      case 2: HAL_GPIO_WritePin(GPIOD,GPIO_PIN_14,0); break;
+      case 3: HAL_GPIO_WritePin(GPIOD,GPIO_PIN_15,0); break;
+    }
+  }
+}
+
+/*----------------------------------------------------------------------------
+  Function that turns on requested LED
+ *----------------------------------------------------------------------------*/
+void LED_Toggle (uint32_t num) {
+  if (num < LED_NUM) {
+    switch (num) {
+      case 0: HAL_GPIO_TogglePin(GPIOD,GPIO_PIN_12); break;
+      case 1: HAL_GPIO_TogglePin(GPIOD,GPIO_PIN_13); break;
+      case 2: HAL_GPIO_TogglePin(GPIOD,GPIO_PIN_14); break;
+      case 3: HAL_GPIO_TogglePin(GPIOD,GPIO_PIN_15); break;
+    }
+  }
+}
+
+/*----------------------------------------------------------------------------
+  Function that outputs value to LEDs
+ *----------------------------------------------------------------------------*/
+void LED_Out(uint32_t value) {
+  int i;
+
+  for (i = 0; i < LED_NUM; i++) {
+    if (value & (1<<i)) {
+      LED_On (i);
+    } else {
+      LED_Off(i);
+    }
+  }
+}
---
+++ keil/tasks_FreeRTOS/RTE/_STM32F407/RTE_Components.h
@@ -0,0 +1,45 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'tasks_FreeRTOS' 
+ * Target:  'STM32F407' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+/*  ARM.FreeRTOS::RTOS:Config:FreeRTOS:10.3.1 */
+#define RTE_RTOS_FreeRTOS_CONFIG        /* RTOS FreeRTOS Config for FreeRTOS API */
+/*  ARM.FreeRTOS::RTOS:Core:Cortex-M:10.3.1 */
+#define RTE_RTOS_FreeRTOS_CORE          /* RTOS FreeRTOS Core */
+/*  ARM.FreeRTOS::RTOS:Heap:Heap_4:10.3.1 */
+#define RTE_RTOS_FreeRTOS_HEAP_4        /* RTOS FreeRTOS Heap 4 */
+/*  ARM.FreeRTOS::RTOS:Timers:10.3.1 */
+#define RTE_RTOS_FreeRTOS_TIMERS        /* RTOS FreeRTOS Timers */
+/*  Keil::Device:STM32Cube Framework:Classic:1.7.9 */
+#define RTE_DEVICE_FRAMEWORK_CLASSIC
+/*  Keil::Device:STM32Cube HAL:Common:1.7.9 */
+#define RTE_DEVICE_HAL_COMMON
+/*  Keil::Device:STM32Cube HAL:Cortex:1.7.9 */
+#define RTE_DEVICE_HAL_CORTEX
+/*  Keil::Device:STM32Cube HAL:DMA:1.7.9 */
+#define RTE_DEVICE_HAL_DMA
+/*  Keil::Device:STM32Cube HAL:GPIO:1.7.9 */
+#define RTE_DEVICE_HAL_GPIO
+/*  Keil::Device:STM32Cube HAL:PWR:1.7.9 */
+#define RTE_DEVICE_HAL_PWR
+/*  Keil::Device:STM32Cube HAL:RCC:1.7.9 */
+#define RTE_DEVICE_HAL_RCC
+/*  Keil::Device:Startup:2.6.3 */
+#define RTE_DEVICE_STARTUP_STM32F4XX    /* Device Startup for STM32F4 */
+
+
+#endif /* RTE_COMPONENTS_H */
